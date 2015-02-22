95754b393c9479ab62ac64dc49d975673f4a9e89
KARTHIK <dilipkumar@Dilips-Mac.local>
2015-02-22 02:07:26 -0800

    cfgbcg

--- iOSSecondApp/ViewController.m
+++ iOSSecondApp/ViewController.m
@@ -13,24 +13,43 @@
 @end
 
 @implementation ViewController
-int flag = 1;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [UIView animateWithDuration:2.0 animations: ^{
+        self.view.backgroundColor = [self colorGiver];
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
 
+-(UIColor*)colorGiver{
+    CGFloat green=(arc4random()%100) *.01;
+    CGFloat blue=(arc4random()%100) * .01;
+    CGFloat red=(arc4random()%100) *.01;
+    UIColor *abc = [UIColor colorWithRed:red green:green blue:blue alpha:1];
+    return abc;
+}
+int flag;
+
 - (IBAction)transform:(UIButton *)sender {
-        CGFloat green=(arc4random()%100) *.01;
-        CGFloat blue=(arc4random()%100) * .01;
-        CGFloat red=(arc4random()%100) *.01;
-        [UIView animateWithDuration:2.0 animations: ^{
-        self.view.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1];
-    }];
-    
+    for(flag = 1; flag < 12; flag++){
+        sleep(5);
+        NSLog(@"%d", flag);
+        self.view.backgroundColor = [self colorGiver];
+        NSLog(@"%@", self.view.backgroundColor);
+        if(flag == 11){
+            flag = 1;
+        }
+        
+    }
 }
 @end
