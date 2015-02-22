40332148b3497726b8103ebbc8f4995087add9a6
KARTHIK <dilipkumar@Dilips-Mac.local>
2015-02-21 23:52:34 -0800

    Initial Commit

---
+++ iOSSecondApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSSecondApp
+//
+//  Created by KARTHIK on 2/21/15.
+//  Copyright (c) 2015 Siva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSSecondApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSSecondApp
+//
+//  Created by KARTHIK on 2/21/15.
+//  Copyright (c) 2015 Siva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
